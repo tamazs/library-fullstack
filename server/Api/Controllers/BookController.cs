c152a3c93b53241ee96ca37e942957c8f7bfd66e
Tamás Major <majortamas313@gmail.com>
2025-09-23 23:30:55 +0200

    book methods done

--- server/Api/Controllers/BookController.cs
+++ server/Api/Controllers/BookController.cs
@@ -1,6 +1,71 @@
-﻿namespace Api.Controllers;
+﻿using Api.DTOs;
+using Api.DTOs.Request;
+using Api.Services;
+using Microsoft.AspNetCore.Mvc;
 
-public class BookController
+namespace Api.Controllers;
+
+[ApiController]
+public class BookController  : ControllerBase
 {
-    
+    private readonly BookService _bookService;
+
+    public BookController(BookService bookService)
+    {
+        _bookService = bookService;
+    }
+
+    [HttpGet(nameof(GetAllBooks))]
+    public async Task<ActionResult<List<BookDto>>> GetAllBooks()
+    {
+        try
+        {
+            var books = await _bookService.GetAllBooks();
+            return Ok(books);
+        }
+        catch (Exception ex)
+        {
+            return NotFound(ex.Message);
+        }
+    }
+
+    [HttpPost(nameof(CreateBook))]
+    public async Task<ActionResult<BookDto>> CreateBook([FromBody] CreateBookRequestDto dto)
+    {
+        try
+        {
+            var book = await _bookService.CreateBook(dto);
+            return Ok(book);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpPut(nameof(UpdateBook))]
+    public async Task<ActionResult<BookDto>> UpdateBook([FromBody] UpdateBookRequestDto dto)
+    {
+        try
+        {
+            var book = await _bookService.UpdateBook(dto);
+            return Ok(book);
+        } catch (Exception ex) 
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpDelete(nameof(DeleteBook))]
+    public async Task<ActionResult<BookDto>> DeleteBook(string id)
+    {
+        try
+        {
+            var book = await _bookService.DeleteBook(id);
+            return Ok(book);
+        } catch (Exception ex) 
+        {
+            return BadRequest(ex.Message);
+        }
+    }
 }
--- server/Api/Services/BookService.cs
+++ server/Api/Services/BookService.cs
@@ -1,6 +1,62 @@
-﻿namespace Api.Services;
+﻿using System.ComponentModel.DataAnnotations;
+using Api.DTOs;
+using Api.DTOs.Request;
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
 
-public class BookService
+namespace Api.Services;
+
+public class BookService (LibraryDbContext  dbContext)
 {
-    
+    public async Task<List<BookDto>> GetAllBooks()
+    {
+        return await dbContext.Books
+            .Include(b => b.Genre)
+            .Include(b => b.Authors)
+            .Select(b => new BookDto(b))
+            .ToListAsync();
+    }
+
+    public async Task<BookDto> CreateBook(CreateBookRequestDto dto)
+    {
+        Validator.ValidateObject(dto, new ValidationContext(dto), true);
+
+        var book = new Book
+        {
+            Id = Guid.NewGuid().ToString(),
+            Title = dto.Title,
+            Pages = dto.Pages,
+            Createdat = DateTime.UtcNow
+        };
+        
+        await dbContext.Books.AddAsync(book);
+        await dbContext.SaveChangesAsync();
+        return new BookDto(book);
+    }
+
+    public async Task<BookDto> UpdateBook(UpdateBookRequestDto dto)
+    {
+        Validator.ValidateObject(dto, new ValidationContext(dto), true);
+
+        var book = await dbContext.Books.FirstAsync(b => b.Id == dto.BookId);
+        await dbContext.Entry(book).Collection(b => b.Authors).LoadAsync();
+        
+        book.Title = dto.Title;
+        book.Pages = dto.Pages;
+        book.Genre = dto.GenreId != null ? await dbContext.Genres.FirstAsync(g => g.Id == dto.GenreId) : null;
+        
+        book.Authors.Clear();
+        dto.AuthorIds.ForEach(id => book.Authors.Add(dbContext.Authors.First(a => a.Id == id)));
+        
+        await dbContext.SaveChangesAsync();
+        return new BookDto(book);
+    }
+
+    public async Task<BookDto> DeleteBook(string id)
+    {
+        var book = await dbContext.Books.FirstAsync(b => b.Id == id);
+        dbContext.Books.Remove(book);
+        await dbContext.SaveChangesAsync();
+        return new BookDto(book);
+    }
 }
